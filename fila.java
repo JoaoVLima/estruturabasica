104f6cf48d642e3ba53dd25454b5fe18feb53c07
JoaoVLima <celu.joaolima@gmail.com>
2025-02-26 12:23:45 -0300

    fila, ainda nao consegui terminar

--- fila.java
+++ fila.java
@@ -19,22 +19,54 @@ public class fila {
             else{
                 return false;
             }
+        }else{
+            return false;
         }
     }
 
     public boolean vazia(){
-
+        if (this.primeiro != -1 & this.ultimo != -1){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public void insere(){
-
+    public void insere(int valor){
+        if (!cheia()){
+            if(vazia()){
+                this.primeiro = 0;
+            }
+            this.ultimo = (this.ultimo + 1) % this.tamanho;
+            this.dados[ultimo] = valor;
+            System.out.println("OK - inserido");
+        }else{
+            System.out.println("ERROR - fila ta cheia");
+        }
     }
 
     public void remove(){
-
+        if (!vazia()){
+            if(this.primeiro != this.ultimo){
+                this.primeiro = (this.primeiro + 1) % this.tamanho;
+            }else{
+                this.primeiro = -1;
+                this.ultimo = -1;
+            }
+            System.out.println("OK - removido");
+        }else{
+            System.out.println("ERROR - fila ta cheia");
+        }
     }
 
     public void imprime(){
-
+        System.out.print("Fila = ");
+        int i = this.primeiro;
+        while(i!=this.ultimo) {
+            System.out.print(this.dados[i]);
+            System.out.print(" ");
+            i = (i + 1) % tamanho;
+        }
+        System.out.println();
     }
 }
