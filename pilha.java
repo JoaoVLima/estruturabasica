07b3088a809bcddd02a1e160548f622db86a623a
JoaoVLima <celu.joaolima@gmail.com>
2025-03-19 11:32:59 -0300

    correcoes do professor, pilha

--- pilha.java
+++ pilha.java
@@ -1,53 +1,55 @@
 
 public class pilha {
-    public int tamanho;
+    public int MAX;
     public int[] dados;
     public int topo;
 
     public pilha() {
-        this.tamanho = 5;
-        this.dados = new int[this.tamanho];
-        this.topo = 0;
+        this.MAX = 5;
+        this.dados = new int[this.MAX];
+        this.topo = -1;
     }
 
-    public void insere(int valor) {
-        if (!this.cheia()) {
-            dados[topo] = valor;
-            this.topo = this.topo + 1;
-            System.out.println("OK - inserido");
-        } else {
+    public void insere(int elemento) {
+        if (this.cheia()) {
             System.out.println("ERROR - pilha ta cheia");
+            return;
         }
+        this.topo = this.topo + 1;
+        this.dados[this.topo] = elemento;
+        System.out.println("OK - inserido");
     }
 
     public boolean cheia() {
-        if (this.topo == this.tamanho) {
-            return true;
-        } else {
-            return false;
-        }
+        return (this.topo == this.MAX - 1);
     }
 
     public boolean vazia() {
-        if (this.topo == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (this.topo == -1);
     }
 
-    public void remove() {
-        if (!this.vazia()) {
-            this.topo = this.topo - 1;
-            System.out.println("OK - removido");
-        } else {
+    public Integer remove() {
+        if (this.vazia()) {
             System.out.println("ERROR - pilha ta vazia");
+            return null;
         }
+        int elemento = this.dados[this.topo];
+        this.topo = this.topo - 1;
+        System.out.println("OK - removido");
+        return elemento;
+    }
+
+    public Integer consultarTopo(){
+        if (this.vazia()){
+            System.out.println("ERROR - pilha ta vazia");
+            return null;
+        }
+        return this.dados[this.topo];
     }
 
     public void imprime() {
         System.out.print("Pilha = ");
-        for (int i = 0; i < this.topo; i++) {
+        for (int i = 0; i <= this.topo; i++) {
             System.out.print(this.dados[i]);
             System.out.print(" ");
         }
